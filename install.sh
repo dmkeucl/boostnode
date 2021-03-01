7b68ae60e9e010f69fda923e3558b83acb5f923b
dmkeucl <78326516+dmkeucl@users.noreply.github.com>
2021-03-01 18:24:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,31 +33,33 @@ then
 	exit 1
 fi
 ## Full install, config and launch of the nym-mixnode
-if [ "$1" = "-i" ]; 
-then
-    while [ ! -d /home/nym1 ] ; 
+for(int $i=1; $i <=5; $i++ ){
+	$nym = "nym"
+	$nym += $i
+	echo $nym
+    while [ ! -d /home/${nym} ] ; 
 	do
-		useradd -U -m -s /sbin/nologin nym1
+		useradd -U -m -s /sbin/nologin ${nym}
 		printf "%b\n\n\n"
-		printf "%b\n\n\n" "${YELLOW} Creating ${WHITE} nym1 user\n\n"
-		if ls -a /home/ | grep nym1 > /dev/null 2>&1
+		printf "%b\n\n\n" "${YELLOW} Creating ${WHITE} ${nym} user\n\n"
+		if ls -a /home/ | grep ${nym} > /dev/null 2>&1
 		then
-			printf "%b\n\n\n" "${WHITE} User ${YELLOW} nym1 ${LGREEN} created ${WHITE} with a home directory at ${YELLOW} /home/nym1/"
+			printf "%b\n\n\n" "${WHITE} User ${YELLOW} ${nym} ${LGREEN} created ${WHITE} with a home directory at ${YELLOW} /home/${nym}/"
 
 		else
-			printf "%b\n\n\n" "${WHITE} Something went ${RED} wrong ${WHITE} and the user ${YELLOW} nym1 ${WHITE}was ${RED} not created."
+			printf "%b\n\n\n" "${WHITE} Something went ${RED} wrong ${WHITE} and the user ${YELLOW} ${nym} ${WHITE}was ${RED} not created."
 		fi
 	done
 	
-    cd /home/nym1/ || printf "%b\n\n\n" "${WHITE}failed sorry"
-    if [ ! -e /home/nym1/nym-mixnode_linux_x86_64 ] ; 
+    cd /home/${nym}/ || printf "%b\n\n\n" "${WHITE}failed sorry"
+    if [ ! -e /home/${nym}/nym-mixnode_linux_x86_64 ] ; 
 	then
 		if	
-			cat /etc/passwd | grep nym1 > /dev/null 2>&1
+			cat /etc/passwd | grep ${nym} > /dev/null 2>&1
 		then
 			printf "%b\n\n\n" "${WHITE} --------------------------------------------------------------------------------"
-			printf "%b\n\n\n" "${YELLOW} Downloading ${WHITE} nym-mixnode1 binaries for the nym1 user ..."
-			cd /home/nym1 && curl -LO https://github.com/nymtech/nym/releases/download/v0.9.2/nym-mixnode_linux_x86_64
+			printf "%b\n\n\n" "${YELLOW} Downloading ${WHITE} nym-mixnode1 binaries for the ${nym} user ..."
+			cd /home/${nym} && curl -LO https://github.com/nymtech/nym/releases/download/v0.9.2/nym-mixnode_linux_x86_64
 			printf "%b\n\n\n"
 			printf "%b\n\n\n" "${WHITE} nym-mixnode1 binaries ${LGREEN} successfully downloaded ${WHITE}!"
 		else
@@ -66,13 +68,12 @@ then
 		fi
 	fi
 
-	#    nym_chmod	
-		
-	if ls -la /home/nym1/ | grep nym-mixnode_linux_x86_64 > /dev/null 2>&1
+	#    nym_chmod			
+	if ls -la /home/${nym}/ | grep nym-mixnode_linux_x86_64 > /dev/null 2>&1
 	then
 		printf "%b\n\n\n" "${WHITE} --------------------------------------------------------------------------------"
-		printf "%b\n\n\n" "${WHITE} Making the nym1 binary ${YELLOW} executable ..."
-		chmod 755 /home/nym1/nym-mixnode_linux_x86_64
+		printf "%b\n\n\n" "${WHITE} Making the ${nym} binary ${YELLOW} executable ..."
+		chmod 755 /home/${nym}/nym-mixnode_linux_x86_64
 		printf "%b\n\n\n" "${LGREEN} Successfully ${WHITE} made the file ${YELLOW} executable !"
 	else
 		printf "%b\n\n\n" "${WHITE} --------------------------------------------------------------------------------"
@@ -80,21 +81,18 @@ then
 	fi
 		
 	#    nym_chown
-
-	chown -R nym1:nym1 /home/nym1/
+	chown -R ${nym}:${nym} /home/${nym}/
 	printf "%b\n\n\n" "${WHITE} --------------------------------------------------------------------------------"
-	printf "%b\n\n\n" "${WHITE} Changed ownership of all conentes in ${YELLOW}/home/nym1/ ${WHITE} to ${YELLOW}nym1:nym1"
-	 
-	 
+	printf "%b\n\n\n" "${WHITE} Changed ownership of all conentes in ${YELLOW}/home/${nym}/ ${WHITE} to ${YELLOW}${nym}:${nym}"
+	 	 
 	#    nym_init
-
 	ip_addr=`curl -sS v4.icanhazip.com`
 	## ahost=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 	ahost=$(ip -6 addr|awk '{print $2}'|grep -P '^(?!fe80)[[:alnum:]]{4}:.*/64'|cut -d '/' -f1)
 	printf "%b\n\n\n" "${WHITE} --------------------------------------------------------------------------------"
 	printf "%b\n\n\n" "${YELLOW} Configuration ${WHITE} file and keys: "
 	if
-	   pwd | grep /home/nym1 > /dev/null 2>&1
+	   pwd | grep /home/${nym} > /dev/null 2>&1
 	then
 	   printf "%b\n\n\n" "${WHITE} Your node name will be ${YELLOW} 'NymMixNode'. ${WHITE} Use it nextime if you restart your server or the node is not running"
 	   printf "%b\n\n\n"
@@ -117,7 +115,7 @@ then
 	   printf "%b\n\n\n" "${WHITE} Ipv6: ${YELLOW} ${ahost} "
 	   printf "%b\n\n\n" "${WHITE}"
 	   sleep 1    
-	   sudo -u nym1 -H ./nym-mixnode_linux_x86_64 init --id 'NymMixNode' --location $location1 --incentives-address $walletx1 --host $ahost --layer $layer1 
+	   sudo -u ${nym} -H ./nym-mixnode_linux_x86_64 init --id 'NymMixNode' --location $location1 --incentives-address $walletx1 --host $ahost --layer $layer1 
 	   printf "%b\n\n\n" "${WHITE} --------------------------------------------------------------------------------"
 	   # borrows a shell for nym user to initialize the node config.
 	   printf "%b\n\n\n"
@@ -137,8 +135,8 @@ then
 		printf '%s\n' "Description=Nym Mixnode (0.9.2)" >> /etc/systemd/system/nym-mixnode1.service
 		printf '%s\n' "" >> /etc/systemd/system/nym-mixnode1.service
 		printf '%s\n' "[Service]" >> /etc/systemd/system/nym-mixnode1.service
-		printf '%s\n' "User=nym1" >> /etc/systemd/system/nym-mixnode1.service
-		printf '%s\n' "ExecStart=/home/nym1/nym-mixnode_linux_x86_64 run --id NymMixNode" >> /etc/systemd/system/nym-mixnode1.service
+		printf '%s\n' "User=${nym}" >> /etc/systemd/system/nym-mixnode1.service
+		printf '%s\n' "ExecStart=/home/${nym}/nym-mixnode_linux_x86_64 run --id NymMixNode" >> /etc/systemd/system/nym-mixnode1.service
 		printf '%s\n' "KillSignal=SIGINT" >> /etc/systemd/system/nym-mixnode1.service				
 		printf '%s\n' "Restart=on-failure" >> /etc/systemd/system/nym-mixnode1.service
 		printf '%s\n' "RestartSec=30" >> /etc/systemd/system/nym-mixnode1.service
@@ -206,5 +204,5 @@ then
     printf "%b\n" "${WHITE}                            You have ${YELLOW} mixed ${WHITE} so far ! "
     printf "%b\n\n\n"
     printf "%b\n\n\n" "${WHITE} --------------------------------------------------------------------------------"
-fi
+}
 
